286e1aa2a09bdf53e0e4cd749430e79f03967ee7
하성준 <haseongjun@haseongjun-ui-MacBookPro.local>
2020-06-19 01:03:42 +0900

    eslint airbnb 적용

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+/* eslint-disable import/extensions */
 import { registerRootComponent } from 'expo';
 
-import App from './App';
+// eslint-disable-next-line import/no-unresolved
+import { App } from './src/App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
