69b48c1c1b930ed0c52bad7d17b7da8ded57741c
하성준 <haseongjun@haseongjun-ui-MacBookPro.local>
2020-06-20 01:07:58 +0900

    add image picker

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable no-alert */
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const logo = require('./assets/logo.png');
@@ -9,7 +11,7 @@ const styles = StyleSheet.create({
 		flex: 1,
 		backgroundColor: '#fff',
 		alignItems: 'center',
-		justifyContent: 'center',
+		justifyContent: 'space-around',
 	},
 	logo: {
 		width: 305,
@@ -21,13 +23,59 @@ const styles = StyleSheet.create({
 		fontSize: 18,
 		marginHorizontal: 15,
 	},
+	button: {
+		backgroundColor: 'blue',
+		padding: 20,
+		borderRadius: 5,
+	},
+	buttonText: {
+		fontSize: 20,
+		color: '#fff',
+	},
+	thumbnail: {
+		width: 300,
+		height: 300,
+		resizeMode: 'contain',
+	},
 });
 
 export default function App(): JSX.Element {
+	const [selectedImage, setSelectedImage] = React.useState(null);
+
+	const openImagePickerAsync = async () => {
+		const permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
+
+		if (permissionResult.granted === false) {
+			alert('Permission to access camera roll is required!');
+			return;
+		}
+
+		const pickerResult = await ImagePicker.launchImageLibraryAsync();
+
+		if (pickerResult.cancelled === true) {
+			return;
+		}
+
+		setSelectedImage({ localUri: pickerResult.uri });
+	};
+
+	if (selectedImage !== null) {
+		return (
+			<View style={styles.container}>
+				<Image source={{ uri: selectedImage.localUri }} style={styles.thumbnail} />
+			</View>
+		);
+	}
+
 	return (
 		<View style={styles.container}>
-			<Image source={logo} style={styles.instructions} />
-			<Text style={styles.logo}>My First React Native App tutorial</Text>
+			<View>
+				<Image source={logo} style={styles.logo} />
+				<Text style={styles.instructions}>My First React Native App tutorial</Text>
+			</View>
+			<TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
+				<Text style={styles.buttonText}>Pick a photo</Text>
+			</TouchableOpacity>
 		</View>
 	);
 }
