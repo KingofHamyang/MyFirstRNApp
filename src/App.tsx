35a0a1edf95ea6dac4b0caa88f2a58779b6a4042
하성준 <haseongjun@haseongjun-ui-MacBookPro.local>
2020-06-20 00:49:54 +0900

    main page loaded

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const logo = require('./assets/logo.png');
 
 const styles = StyleSheet.create({
 	container: {
@@ -8,12 +11,23 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 		justifyContent: 'center',
 	},
+	logo: {
+		width: 305,
+		height: 159,
+		marginBottom: 10,
+	},
+	instructions: {
+		color: '#888',
+		fontSize: 18,
+		marginHorizontal: 15,
+	},
 });
 
 export default function App(): JSX.Element {
 	return (
 		<View style={styles.container}>
-			<Text>Open up App.tsx to start working on your app!</Text>
+			<Image source={logo} style={styles.instructions} />
+			<Text style={styles.logo}>My First React Native App tutorial</Text>
 		</View>
 	);
 }
